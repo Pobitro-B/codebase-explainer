feec2bce34cd59f0942e2f04db1601c962346aa2
Pobitro-B <bhattacharyapobitro@gmail.com>
2026-05-28 21:26:32 +0530

    add file format id

--- backend/app/services/filereader.py
+++ backend/app/services/filereader.py
@@ -1,5 +1,80 @@
 from pathlib import Path
 
+EXTENSION_MAP = {
+  # Web
+  "js": "javascript",
+  "mjs": "javascript",
+  "cjs": "javascript",
+  "jsx": "javascriptreact",
+
+  'ts': "typescript",
+  "tsx": "typescriptreact",
+
+  "html": "html",
+  "css": "css",
+  "scss": "scss",
+  "svelte": "svelte",
+  "vue": "vue",
+
+  # Backend
+  "py": "python",
+  "java": "java",
+  "go": "go",
+  "rs": "rust",
+  "php": "php",
+  "rb": "ruby",
+
+  # Systems
+  "c": "c",
+  "h": "c",
+  "cpp": "cpp",
+  "cc": "cpp",
+  "cxx": "cpp",
+  "hpp": "cpp",
+
+  "cs": "csharp",
+
+  # Mobile
+  "kt": "kotlin",
+  "swift": "swift",
+  "dart": "dart",
+
+  # Scripting
+  "sh": "bash",
+  "bash": "bash",
+  "zsh": "bash",
+  "ps1": "powershell",
+
+  # Data / Config
+  "json": "json",
+  "yaml": "yaml",
+  "yml": "yaml",
+  "toml": "toml",
+  "xml": "xml",
+
+  # Markup
+  "md": "markdown",
+
+  # SQL
+  "sql": "sql",
+
+  # Functional
+  "hs": "haskell",
+  "clj": "clojure",
+  "scm": "scheme",
+  "lisp": "lisp",
+
+  # Misc
+  "lua": "lua",
+  'r': "r",
+  "scala": "scala",
+  "pl": "perl",
+
+  # Notebooks
+  "ipynb": "json",
+  "txt": "text",
+}
+
 def file_contents(path: str):
     """_summary_
 
@@ -11,11 +86,15 @@ def file_contents(path: str):
     """
     if path == "HOME_PAGE":
         path = "./app/static/home.txt"
-    print(Path(path).resolve())
     with open(Path(path).resolve(), "r", encoding='utf-8') as f:
         content = f.readlines()
+    try:
+        lang = EXTENSION_MAP[Path(path).name.split(".")[-1]]
+    except(KeyError):
+        lang = "text"    
     return {
         "name": Path(path).name,
         "path": path,
-        "content": content
+        "content": content,
+        "language": lang
     }
--- desktop/src/components/FileArea.tsx
+++ desktop/src/components/FileArea.tsx
@@ -1,4 +1,6 @@
 import "./FileArea.css";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 export default function FileArea(props) {
   const name = props.fileContent["name"];
@@ -11,7 +13,14 @@ export default function FileArea(props) {
       </div>
       <div className="code-area">
         {contents.map((line) => (
-          <div className="code-line">{line}</div>
+          <div className="code-line">
+            <SyntaxHighlighter
+              language={props.fileContent["language"]}
+              style={dark}
+            >
+              {line}
+            </SyntaxHighlighter>
+          </div>
         ))}
       </div>
     </div>
